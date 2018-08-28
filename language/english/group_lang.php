b20467056b0ea07b28ae281fc1cc463bbde2de3a
kleber <ruzkleber@gmail.com>
2018-08-27 21:57:06 -0300

    add lang lines and translate.

--- language/english/group_lang.php
+++ language/english/group_lang.php
@@ -1,40 +1,43 @@
-<?php defined('BASEPATH') || exit('No direct script access allowed');
+<?php defined ('BASEPATH') || exit ('No direct access to allowed script');
 
+$lang['group_manage'] = 'Manage Group';
+$lang['group_edit'] = 'Edit';
+$lang['group_true'] = 'True';
+$lang['group_false'] = 'False';
+$lang['group_create'] = 'Create';
+$lang['group_list'] = 'List';
+$lang['group_new'] = 'New';
+$lang['group_edit_text'] = 'Edit this to suit your needs';
+$lang['group_no_records'] = 'There are no groups registered.';
+$lang['group_create_new'] = 'Create a new Group.';
+$lang['group_create_success'] = 'Successfully created group.';
+$lang['group_create_failure'] = 'There was a problem creating the Group:';
+$lang['group_create_new_button'] = 'Create new group';
+$lang['group_invalid_id'] = 'Invalid Group ID.';
+$lang['group_edit_success'] = 'Group successfully saved.';
+$lang['group_edit_failure'] = 'There was a problem saving the Group:';
+$lang['group_delete_success'] = 'successfully deleted record (s).';
+$lang['group_delete_failure'] = 'Could not delete record:';
+$lang['group_delete_error'] = 'You have not selected any records to delete.';
+$lang['group_actions'] = 'Actions';
+$lang['group_cancel'] = 'Cancel';
+$lang['group_delete_record'] = 'Delete this Group';
+$lang['group_delete_confirm'] = 'Do you really want to delete this group (s)?';
+$lang['group_edit_heading'] = 'Edit Group';
+$lang['group_desc_form_create'] = 'Add / edit contact groups.';
 
+// Create / Edit Buttons
+$lang['group_action_edit'] = 'Save Group';
+$lang['group_action_create'] = 'Add group';
 
-$lang['group_manage']      = 'Manage Group';
-$lang['group_edit']        = 'Edit';
-$lang['group_true']        = 'True';
-$lang['group_false']       = 'False';
-$lang['group_create']      = 'Create';
-$lang['group_list']        = 'List';
-$lang['group_new']       = 'New';
-$lang['group_edit_text']     = 'Edit this to suit your needs';
-$lang['group_no_records']    = 'There are no group in the system.';
-$lang['group_create_new']    = 'Create a new Group.';
-$lang['group_create_success']  = 'Group successfully created.';
-$lang['group_create_failure']  = 'There was a problem creating the group: ';
-$lang['group_create_new_button'] = 'Create New Group';
-$lang['group_invalid_id']    = 'Invalid Group ID.';
-$lang['group_edit_success']    = 'Group successfully saved.';
-$lang['group_edit_failure']    = 'There was a problem saving the group: ';
-$lang['group_delete_success']  = 'record(s) successfully deleted.';
-$lang['group_delete_failure']  = 'We could not delete the record: ';
-$lang['group_delete_error']    = 'You have not selected any records to delete.';
-$lang['group_actions']     = 'Actions';
-$lang['group_cancel']      = 'Cancel';
-$lang['group_delete_record']   = 'Delete this Group';
-$lang['group_delete_confirm']  = 'Are you sure you want to delete this group?';
-$lang['group_edit_heading']    = 'Edit Group';
-
-// Create/Edit Buttons
-$lang['group_action_edit']   = 'Save Group';
-$lang['group_action_create']   = 'Create Group';
-
-
+$lang['group_module_name'] = 'Groups';
 
 // Fields
 $lang['group_field_group_name'] = 'Name';
 $lang['group_field_description'] = 'Description';
 $lang['group_field_number_contacts'] = 'Contacts';
-$lang['group_field_created_on'] = 'Created on';
+$lang['group_field_created_on'] = 'Created in';
+$lang['group_field_parent_group'] = 'Parent group';
+$lang['group_field_contact_join_on'] = 'From';
+
+$lang['group_placeholder_description'] = 'Description of contact group ...';
--- language/portuguese_br/group_lang.php
+++ language/portuguese_br/group_lang.php
@@ -1,40 +1,38 @@
-<?php defined('BASEPATH') || exit('No direct script access allowed');
+<?php defined ('BASEPATH') || exit ('Nenhum acesso direto ao script permitido');
 
+$lang['group_manage'] = 'Gerenciar Grupo';
+$lang['group_edit'] = 'Editar';
+$lang['group_true'] = 'Verdadeiro';
+$lang['group_false'] = 'Falso';
+$lang['group_create'] = 'Criar';
+$lang['group_list'] = 'Listar';
+$lang['group_new'] = 'Novo';
+$lang['group_edit_text'] = 'Edite isso para atender às suas necessidades';
+$lang['group_no_records'] = 'Não há grupos cadastrados.';
+$lang['group_create_new'] = 'Cria um novo Grupo.';
+$lang['group_create_success'] = 'Grupo criado com sucesso.';
+$lang['group_create_failure'] = 'Ocorreu um problema ao criar o Grupo:';
+$lang['group_create_new_button'] = 'Criar novo grupo';
+$lang['group_invalid_id'] = 'ID do Grupo inválido.';
+$lang['group_edit_success'] = 'Grupo salvo com sucesso.';
+$lang['group_edit_failure'] = 'Ocorreu um problema ao salvar o Grupo:';
+$lang['group_delete_success'] = 'registro (s) eliminado (s) com sucesso.';
+$lang['group_delete_failure'] = 'Não foi possível deletar o registro:';
+$lang['group_delete_error'] = 'Você não selecionou nenhum registro para deletar.';
+$lang['group_actions'] = 'Ações';
+$lang['group_cancel'] = 'Cancelar';
+$lang['group_delete_record'] = 'Excluir este Grupo';
+$lang['group_delete_confirm'] = 'Quer mesmo excluir este (s) grupo (s)?';
+$lang['group_edit_heading'] = 'Editar Grupo';
+$lang['group_desc_form_create'] = 'Adicionar / editar grupos de contatos.';
 
-
-$lang['group_manage']      = 'Manage Grupo';
-$lang['group_edit']        = 'Edit';
-$lang['group_true']        = 'True';
-$lang['group_false']       = 'False';
-$lang['group_create']      = 'Criar ';
-$lang['group_list']        = 'Listar';
-$lang['group_new']       = 'New';
-$lang['group_edit_text']     = 'Edit this to suit your needs';
-$lang['group_no_records']    = 'There are no Grupo in the system.';
-$lang['group_create_new']    = 'Criar a new Grupo.';
-$lang['group_create_success']  = 'Grupo criado com sucesso.';
-$lang['group_create_failure']  = 'There was a problem creating the Grupo: ';
-$lang['group_create_new_button'] = 'Criar New Grupo';
-$lang['group_invalid_id']    = 'Invalid Grupo ID.';
-$lang['group_edit_success']    = 'Group successfully saved.';
-$lang['group_edit_failure']    = 'There was a problem saving the Grupo: ';
-$lang['group_delete_success']  = 'record(s) successfully deleted.';
-$lang['group_delete_failure']  = 'We could not delete the record: ';
-$lang['group_delete_error']    = 'You have not selected any records to delete.';
-$lang['group_actions']     = 'Actions';
-$lang['group_cancel']      = 'Cancelar';
-$lang['group_delete_record']   = 'Delete this Grupo';
-$lang['group_delete_confirm']  = 'Quer mesmo excluir este(s) grupo(s)?';
-$lang['group_edit_heading']    = 'Edit Grupo';
-$lang['group_desc_form_create']    = 'Adicione/edite groupos de contatos.';
-
-// Create/Edit Buttons
-$lang['group_action_edit']   = 'Save Grupo';
-$lang['group_action_create']   = 'Adicionar grupo';
+// Cria / Edita Botões
+$lang['group_action_edit'] = 'Salvar Grupo';
+$lang['group_action_create'] = 'Adicionar grupo';
 
 $lang['group_module_name'] = 'Grupos';
 
-// Fields
+// Campos
 $lang['group_field_group_name'] = 'Nome';
 $lang['group_field_description'] = 'Descrição';
 $lang['group_field_number_contacts'] = 'Contatos';
@@ -42,4 +40,4 @@ $lang['group_field_created_on'] = 'Criado em';
 $lang['group_field_parent_group'] = 'Grupo pai';
 $lang['group_field_contact_join_on'] = 'Desde';
 
-$lang['group_placeholder_description'] = 'Descreva o grupo de contatos...';
+$lang['group_placeholder_description'] = 'Descrição do grupo de contatos ...';
