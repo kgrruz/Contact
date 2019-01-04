42c43c675695f2a62f3709e105448f1466e4d25c
kleber <ruzkleber@gmail.com>
2019-01-04 14:39:39 -0200

    Serialize before insert setting.

--- migrations/002_Install_contact.php
+++ migrations/002_Install_contact.php
@@ -105,7 +105,7 @@ class Migration_Install_contact extends Migration
 		$data_st = array(
 			'name' => 'contact.module_update',
 			'module' => 'contact',
-			'value' => 'a:3:{s:9:"timestamp";i:'.time().';s:7:"version";s:10:"'.$version['version'].'";s:6:"update";i:0;}'
+			'value' => serialize(array("timestamp"=>time(),"version"=>$version['version'],"update"=>0))
 		);
 
 		$this->db->insert("settings",$data_st);
