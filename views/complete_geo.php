d277568ddd6ef5a196b69a61d56b49a8e48fd151
kleber <ruzkleber@gmail.com>
2019-08-29 10:55:44 -0300

    Fix form action url. Add event variable to get html elements.

--- views/complete_geo.php
+++ views/complete_geo.php
@@ -1,22 +1,13 @@
-<h3 class="my-3"><?php echo lang('contact_customer_check_position'); ?></h3>
 
-<?php echo form_open('contact/customer/complete_geo'); ?>
+
+<?php echo form_open($this->uri->uri_string()); ?>
 
 <input type="hidden" id="adress" value="<?php echo (isset($meta->adress))? $meta->adress:''; ?>" />
 <input type="hidden" id="city" value="<?php echo (isset($meta->city))? $meta->city:''; ?>" />
 <input type="hidden" id="country" value="<?php echo (isset($meta->city))? $meta->country:''; ?>" />
 <input type="hidden" id="neibor" value="" />
 
-<div class="card">
-<div class="card-header">Mapa</div>
-<div id="map"></div>
-</div>
-
-<div id="click_address"></div>
-
-<input type="hidden" name="lat" id="lat" />
-<input type="hidden" name="lng" id="lng" />
-
+<?php echo $data_html; ?>
 <hr>
 
 <input type="submit" name="save" value="<?php echo lang('contact_customer_save_coordinate'); ?>" class="btn btn-success" />
