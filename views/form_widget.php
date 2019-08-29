22604a49c7200dce9799e8a29a1952b5721f6082
kleber <ruzkleber@gmail.com>
2019-08-29 10:57:10 -0300

    Set selected value on edit form.

--- views/form_widget.php
+++ views/form_widget.php
@@ -2,12 +2,11 @@
     <?php echo form_label($label . lang('bf_form_label_required'), 'contact', array('class' => 'control-label')); ?>
 
 <select class="form-control form-control-sm" data-placeholder="<?php echo lang("contact_placeholder"); ?>" id="contact_id" name="contact_id" >
-  <option></option>
-  <?php foreach($contacts as $contact){ ?>
-  <option value="<?php echo $contact->id_contact; ?>" data-contact="<?php echo $contact->display_name; ?>"  data-img="<?php echo contact_avatar($contact->email, 20,null, false,'thumb'); ?>" <?php echo (isset($contact_id) and $contact_id == $contact->id_contact)? 'selected':''; ?> >
-     <?php echo $contact->display_name; ?>
-   </option>
-<?php } ?>
+  <?php if($contact){ ?>
+  <option selected value="<?php echo $contact->id_contact; ?>"><?php echo $contact->display_name; ?></option>
+  <?php }else{ ?>
+  <option><?php echo lang("contact_placeholder"); ?></option>
+  <?php } ?>
 </select>
 
 </div>
