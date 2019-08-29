a7e7f78168d94301ad193ad179d1a77897ab51da
kleber <ruzkleber@gmail.com>
2019-08-29 10:58:56 -0300

    Add public profile page. Fix routes.

--- controllers/Contact.php
+++ controllers/Contact.php
@@ -4,8 +4,6 @@
  * Contact controller
  */
 class Contact extends Front_Controller{
-
-
     /**
      * Constructor
      *
@@ -20,30 +18,84 @@ class Contact extends Front_Controller{
         $this->load->model('contact/contact_model');
         $this->load->library('users/auth');
 
-        $this->customer = $this->user_model->find_contact_user($this->auth->user_id());
-
       }
 
     
 
-      public function complete_geo(){
+      public function profile($id){
 
         $this->authenticate();
 
-        Assets::add_module_css('contact','ol.css');
-        Assets::add_module_js('contact','ol.js');
+            if (empty($id)) {
+                Template::set_message(lang('contact_invalid_id'), 'danger');
+                redirect('contacts');
+            }
 
-        Assets::add_module_css('contact','ol-geocoder.min.css');
-        Assets::add_module_js('contact','ol-geocoder.js');
+            if($id = $this->contact_model->find_by('slug_contact',$id)->id_contact){
 
-        Assets::add_module_js('contact','map_widget.js');
+            $contact = $this->contact_model->find_user_and_meta($id);
+
+            if($contact->deleted){
+
+              Template::set_message(lang('contact_is_deleted'), 'danger');
+              Template::redirect('contacts');
+
+            }
+
+            $modules = Modules::list_modules(true);
+
+            $tabs = array();
+
+              foreach($modules as $module) :
+                $config = Modules::config($module);
+                if(isset($config['tab_contact']) and $config['tab_contact']['url']){
+                array_push($tabs,$config['tab_contact']);
+             }
+
+            endforeach;
+
+            if(count($tabs)){
+
+            $function = $this->uri->segment(3,$tabs[0]['url']);
+
+            $data = array('function'=>$function,'view_page'=>'','contact_type'=>'','id_contact'=>$id,'data_table'=>'','slug'=>$contact->slug_contact);
+
+            Events::trigger('show_profile_contact',$data);
+            Template::set('function_tab',$function);
+
+            Template::set('data',$data['data_table']);
+            Template::set('contact_type',$data['contact_type']);
+            Template::set('id_contact',$data['id_contact']);
+            Template::set('view_page', $data['view_page']);
+
+            }
+
+
+            Template::set('tabs',$tabs);
+            Template::set('users_access', $this->db->join("users","users.id = contacts_users.user_id","left")->where("contacts_users.contact_id",$data['id_contact'])->get("contacts_users"));
+            Template::set('contact', $contact);
+            Template::set('toolbar_title', $contact->display_name);
+            Template::render();
+
+          }else{
+
+            Template::set_message(lang('contact_invalid_id'), 'danger');
+            redirect('contact');
+          }
+
+      }
+
+
+      public function complete_geo($idc){
+
+        $this->authenticate();
 
         if (isset($_POST['save'])) {
 
           $meta_data_lat = array(
             'meta_key'=>'lat',
             'meta_value'=>$this->input->post('lat'),
-            'contact_id'=>$this->customer
+            'contact_id'=>$idc
           );
 
           $this->db->insert('contact_meta',$meta_data_lat);
@@ -51,7 +103,7 @@ class Contact extends Front_Controller{
           $meta_data_lng = array(
             'meta_key'=>'lng',
             'meta_value'=>$this->input->post('lng'),
-            'contact_id'=>$this->customer
+            'contact_id'=>$idc
           );
 
           $this->db->insert('contact_meta',$meta_data_lng);
@@ -65,7 +117,7 @@ class Contact extends Front_Controller{
         Events::trigger('show_geo_complete',$data_html);
         Template::set('data_html',$data_html['html']);
 
-        Template::set('meta',$this->contact_model->find_meta_for($this->customer,array('country','city','adress')));
+        Template::set('meta',$this->contact_model->find_meta_for($idc,array('country','city','adress')));
         Template::set('toolbar_title', lang('contact_customer_check_position'));
         Template::render();
 
---
+++ views/profile.php
@@ -0,0 +1,104 @@
+
+<div class="row">
+  <div class="col-md-3">
+
+    <div class="card">
+
+        <?php echo contact_avatar($contact->email, 224,'card-img-top img-fluid w-100',true,'profile_photo');  ?>
+
+  <div class="card-body">
+    <h4 class="card-title"><?php echo ($contact->contact_type == 1)? '<i class="fa fa-user" aria-hidden="true"></i>':'<i class="fa fa-building" aria-hidden="true"></i>'; ?> <?php echo (isset($contact->is_user))? '<i class="fa fa-key"></i>':''; ?> <?php echo $contact->display_name; ?> </h4>
+    <?php echo anchor('contact/content/edit/'.$contact->slug_contact,'<i class="fa fa-edit" aria-hidden="true"></i> '.lang('contact_action_edit')); ?>
+      <?php echo (!empty($contact->job_role))? '<p class="card-text">'.$contact->job_role.'</p>':''; ?>
+    <p class="card-text"><?php echo mailto($contact->email); ?></p>
+  </div>
+  <ul class="list-group list-group-flush">
+
+    	<?php if(isset($contact->country)){ ?>
+				<li class="list-group-item">
+				<i class="fa fa-map-marker" aria-hidden="true"></i>
+	 <?php echo (isset($contact->city))? $contact->city:lang("contact_city_indef"); ?>,  <?php echo (isset($contact->state))? $contact->state:''; ?><br/>
+	 <?php echo (isset($contact->neibor) and !empty($contact->neibor))? $contact->neibor.'<br/>':''; ?>
+	 <?php echo (isset($contact->adress) and !empty($contact->adress))? $contact->adress:''; ?><?php echo (isset($contact->num_adress) and !empty($contact->num_adress))? '-'.$contact->num_adress:''; ?>
+	  </li>
+	<?php } ?>
+
+    <?php echo (!empty($contact->phone))? '<li class="list-group-item">'.$contact->phone.'</li>':''; ?>
+    <?php echo (!empty($contact->phone_2))? '<li class="list-group-item">'.$contact->phone_2.'</li>':''; ?>
+
+    <?php if(isset($contact->company) and $contact->company != 0){ ?>
+    <?php $company = $this->contact_model->find($contact->company);  ?>
+    <li class="list-group-item"><?php echo anchor('admin/contact/content/profile/'.$company->slug_contact,'<i class="fa fa-building" aria-hidden="true"></i> '.$company->display_name); ?></li>
+<?php } ?>
+
+<?php if(count($tabs)){ ?>
+
+    <?php foreach($tabs as $tab){ ?>
+      <?php if(isset($tab['contact_type']) and in_array($contact->contact_type,$tab['contact_type']) or !isset($tab['contact_type'])){ ?>
+
+        <a href="<?php echo base_url().'admin/contact/content/profile/'.$contact->slug_contact.'/'.$tab['url']; ?>"  class="list-group-item list-group-item-action <?php echo ($function_tab == $tab['url'])? 'active':''; ?> "> <?php echo $tab['label'][$current_user->language]; ?> </a>
+
+    <?php } ?>
+  <?php } ?>
+<?php } ?>
+
+  </ul>
+
+</div>
+</div>
+
+<div class="col-md-9">
+
+  <?php if($users_access->num_rows()){ ?>
+
+  <div class="card mb-3">
+    <div class="card-header"><i class="fa fa-key"></i>&nbsp;<?php echo lang('contact_access_by'); ?></div>
+        <ul class="list-group list-group-flush">
+          <div class="row">
+          <?php foreach($users_access->result() as $us){ ?>
+            <div class="col-md-6">
+            <li class="list-group-item">
+              <div class="media">
+                <?php echo user_avatar($us->photo_avatar,$us->email, 60,'d-flex mr-3', true,'thumbs'); ?>
+                <div class="media-body row">
+                  <div class="col-md-9">
+                  <h5 class="mt-0"><?php echo anchor($us->username,ellipsize($us->display_name,30)); ?></h5>
+                <span class="badge badge-success" ><?php echo role_user_by_id($us->role_id); ?></span>
+                </div>
+                        <div class="col-md-3 my-auto">
+                          <?php echo ($this->auth->has_permission("Contact.Content.Create"))? anchor("contact/content/remove_access/".$us->id_access_key,'<i class="fa fa-trash"></i>','data-message="'.lang("contact_confirm_remove_access").'" class="btn btn-light exc_bot" '):''; ?>
+              </div>
+              </div>
+              </div>
+            </li>  </div>
+          <?php } ?>
+          </div>
+        </ul>
+    </div>
+
+  <?php } ?>
+
+
+
+  <?php if(is_array($contact_type) and !in_array($contact->contact_type,$contact_type)){ ?>
+  <div class="card">
+<div class="card-body text-center">
+    <h4 class="card-title"><?php echo lang('contact_nothing_show_title'); ?></h4>
+      <p class="card-text"><?php echo lang('contact_nothing_show_desc'); ?></p>
+
+    </div>
+    </div>
+
+  <?php }else{ ?>
+
+   <?php $this->load->view($view_page); ?>
+
+ <?php } ?>
+
+  </div>
+
+</div>
+</div>
+</div>
+
+<script> var contact_id = <?php echo $contact->id_contact; ?>; </script>
