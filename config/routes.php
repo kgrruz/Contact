ee2ff6d6b5449f5bd3836bbe94bcf7ea4fec5396
kgr <admin@wgstor.com>
2020-04-01 14:56:02 -0300

    Remove unnecessary files.

--- config/routes.php
+++ config/routes.php
@@ -1,8 +1,13 @@
 <?php defined('BASEPATH') || exit('No direct script access allowed');
 
 $route[] = '';
-Route::any('contato/(:any)', 'contact/profile/$1');
-Route::any('contato/(:any)/(:any)', 'contact/profile/$1/$2');
-Route::any('contato/(:any)/(:any)/(:any)', 'contact/profile/$1/$2/$3');
+Route::any('add_contato', 'contact/content/create');
+Route::any('add_contato/(:num)', 'contact/content/create/$1');
+Route::any('add_contato/(:num)/(:num)', 'contact/content/create/$1/$2');
+Route::any('contatos', 'contact/content/index');
+Route::any('contatos/(:num)', 'contact/content/index/$1');
+Route::any('contato/(:any)', 'contact/content/profile/$1');
+Route::any('contato/(:any)/(:any)', 'contact/content/profile/$1/$2');
+Route::any('contato/(:any)/(:any)/(:any)', 'contact/content/profile/$1/$2/$3');
 Route::any('group_related_contact', 'contact/group/contacts_related');
 $route = Route::map($route);
--- views/contact/extend_profile.php
+++
@@ -1,1 +0,0 @@
-  Other contact info
