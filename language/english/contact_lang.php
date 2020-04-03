19895f4db15eec88fdc443ddce23dfb3e561dd4c
kgr <admin@wgstor.com>
2020-04-03 14:31:40 -0300

    Add/fix translations.

--- language/english/contact_lang.php
+++ language/english/contact_lang.php
@@ -1,82 +1,134 @@
 <?php defined('BASEPATH') || exit('No direct script access allowed');
 
-
-$lang['contact_manage']      = 'Manage Contact';
-$lang['contact_edit']        = 'Edit';
-$lang['contact_true']        = 'True';
-$lang['contact_false']       = 'False';
-$lang['contact_create']      = 'Create';
-$lang['contact_list']        = 'List';
-$lang['contact_new']       = 'New';
-$lang['contact_edit_text']     = 'Edit this to suit your needs';
-$lang['contact_no_records']    = 'There are no contact in the system.';
-$lang['contact_create_new']    = 'Create a new Contact.';
-$lang['contact_create_success']  = 'Contact successfully created.';
-$lang['contact_create_failure']  = 'There was a problem creating the contact: ';
-$lang['contact_create_new_button'] = 'Create New Contact';
-$lang['contact_invalid_id']    = 'Invalid Contact ID.';
-$lang['contact_edit_success']    = 'Contact successfully saved.';
-$lang['contact_edit_failure']    = 'There was a problem saving the contact: ';
-$lang['contact_delete_success']  = 'record(s) successfully deleted.';
-$lang['contact_delete_failure']  = 'We could not delete the record: ';
-$lang['contact_delete_error']    = 'You have not selected any records to delete.';
-$lang['contact_actions']     = 'Actions';
-$lang['contact_cancel']      = 'Cancel';
-$lang['contact_delete_record']   = 'Delete this Contact';
-$lang['contact_delete_confirm']  = 'Are you sure you want to delete this contact?';
-$lang['contact_edit_heading']    = 'Edit Contact';
-
-// Create/Edit Buttons
-$lang['contact_action_edit']   = 'Save Contact';
-$lang['contact_action_create']   = 'Create Contact';
+$lang['contact_manage'] = 'Manage contact';
+$lang['contact_edit'] = 'Edit';
+$lang['contact_true'] = 'True';
+$lang['contact_false'] = 'False';
+$lang['contact_create'] = 'Add';
+$lang['contact_list'] = 'List';
+$lang['contact_new'] = 'New';
+$lang['contact_edit_text'] = 'Edit this as needed';
+$lang['contact_no_records'] = 'There are no system contacts.';
+$lang['contact_create_new'] = 'Add a new contact.';
+$lang['contact_create_success'] = 'Contact created successfully.';
+$lang['contact_create_failure'] = 'There was a problem creating the contact:';
+$lang['contact_create_new_button'] = 'Add new contact';
+$lang['contact_invalid_id'] = 'Invalid Contact ID.';
+$lang['contact_edit_success'] = 'Contact saved successfully.';
+$lang['contact_edit_failure'] = 'There was a problem saving contact:';
+$lang['contact_delete_account_confirm'] = 'Are you sure you want to delete the contact (s):';
+$lang['contact_delete_success'] = 'deleted successfully.';
+$lang['contact_delete_failure'] = 'It was not possible to delete the record:';
+$lang['contact_delete_error'] = 'You have not selected any records to delete.';
+$lang['contact_actions'] = 'Actions';
+$lang['contact_cancel'] = 'Cancel';
+$lang['contact_delete_record'] = 'Delete this contact';
+$lang['contact_delete_confirm'] = 'Are you sure you want to delete this contact?';
+$lang['contact_is_deleted'] = 'Contact is disabled.';
+$lang['contact_edit_heading'] = 'Edit contact';
+$lang['parent_user_account'] = 'Associated account';
+$lang['contact_settings'] = 'Contact settings';
+$lang['contact_general_settings'] = 'General';
+$lang['contact_all_types'] = 'Everyone';
+$lang['contact_company'] = 'Company';
+$lang['contact_contact'] = 'Person';
+$lang['contact_contact_name'] = 'Contact';
+$lang['contact_nothing_show_title'] = '';
+$lang['contact_nothing_show_desc'] = '';
+$lang['contact_has_user'] = 'The contact has a related user. It cannot be removed. ';
+$lang['contact_city_indef'] = 'No city';
+$lang['contact_access_to'] = 'Access to contacts';
+$lang['contact_access_by'] = 'Users with access';
+$lang['contact_miss_info'] = 'Not informed.';
+// Create / Edit Buttons
+$lang['contact_action_edit'] = 'Edit contact';
+$lang['contact_action_create'] = 'Register contact';
 
 // Activities
-$lang['contact_act_create_record'] = 'Created record with ID';
-$lang['contact_act_edit_record'] = 'Updated record with ID';
-$lang['contact_act_delete_record'] = 'Deleted record with ID';
+$lang['contact_act_create_record'] = 'Added contact';
+$lang['contact_act_edit_record'] = 'Edited the contact';
+$lang['contact_act_delete_record'] = 'Deleted the contact';
 
-//Listing Specifics
-$lang['contact_records_empty']    = 'No records found that match your selection.';
-$lang['contact_errors_message']    = 'Please fix the following errors:';
+// Listing Specifics
+$lang['contact_records_empty'] = 'No record found.';
+$lang['contact_errors_message'] = 'Please correct the following errors:';
 
 // Column Headings
-$lang['contact_column_display_name'] = 'display_name';
-$lang['contact_column_email'] = 'email';
-$lang['contact_column_phone'] = 'phone';
-$lang['contact_column_timezone'] = 'timezone';
-$lang['contact_column_contact_type'] = 'type';
-$lang['contact_column_adress'] = 'adress';
-$lang['contact_column_country'] = 'country';
-$lang['contact_column_state'] = 'state';
-$lang['contact_column_city'] = 'city';
-$lang['contact_column_neibor'] = 'Neibor';
-$lang['contact_column_num_adress'] = 'adress number';
-$lang['contact_column_created']  = 'Created';
-$lang['contact_column_deleted']  = 'Deleted';
-$lang['contact_column_modified'] = 'Modified';
-$lang['contact_column_deleted_by'] = 'Deleted By';
-$lang['contact_column_created_by'] = 'Created By';
-$lang['contact_column_modified_by'] = 'Modified By';
+$lang['contact_column_display_name'] = 'Name';
+$lang['contact_column_email'] = 'Email';
+$lang['contact_column_phone'] = 'Telephone';
+$lang['contact_column_timezone'] = 'Timezone';
+$lang['contact_column_contact_type'] = 'Type';
+$lang['contact_column_adress'] = 'Address';
+$lang['contact_column_country'] = 'Country';
+$lang['contact_column_state'] = 'State';
+$lang['contact_column_city'] = 'City';
+$lang['contact_column_neibor'] = 'Neighborhood';
+$lang['contact_column_num_adress'] = 'No.';
+$lang['contact_column_created'] = 'Created';
+$lang['contact_column_deleted'] = 'Deleted';
+$lang['contact_column_modified'] = 'Updated';
+$lang['contact_column_job_role'] = 'Position';
+
 
 // Module Details
-$lang['contact_module_name'] = 'Contact';
+$lang['contact_module_name'] = 'contact';
 $lang['contact_module_description'] = 'Manage contacts';
-$lang['contact_area_title'] = 'Contact';
+$lang['contact_area_title'] = 'contact';
 
 // Fields
-$lang['contact_field_display_name'] = 'display_name';
-$lang['contact_field_email'] = 'email';
-$lang['contact_field_phone'] = 'phone';
-$lang['contact_field_timezone'] = 'timezone';
-$lang['contact_field_contact_type'] = 'type';
-$lang['contact_field_adress'] = 'adress';
-$lang['contact_field_country'] = 'country';
-$lang['contact_field_state'] = 'state';
-$lang['contact_field_city'] = 'city';
-$lang['contact_field_neibor'] = 'neibor';
-$lang['contact_field_num_adress'] = 'adress number';
+$lang['contact_field_display_name'] = 'Name';
+$lang['contact_field_phone'] = 'Telephone';
+$lang['contact_field_email'] = 'Email';
+$lang['contact_field_country'] = 'Country';
+$lang['contact_field_city'] = 'City';
+$lang['contact_field_neibor'] = 'Neighborhood';
+$lang['contact_field_state'] = 'State';
+$lang['contact_field_adress'] = 'Address';
+$lang['contact_field_num_adress'] = 'No.';
+
 $lang['contact_field_cpf'] = 'SSN';
-$lang['contact_field_cnpj'] = 'TIN';
-$lang['contact_field_role_job'] = 'role job';
-$lang['contact_field_size'] = 'company size';
-$lang['contact_field_group'] = 'Groups';
+$lang['contact_field_cnpj'] = 'ENI/Company ID';
+$lang['contact_field_size'] = 'Postage';
+
+$lang['contact_field_postcode'] = 'Zip code';
+$lang['contact_field_fantasy_name'] = 'Invented name';
+$lang['contact_field_created_on'] = 'Created on';
+$lang['contact_field_birthday'] = 'Birthday';
+$lang['contact_field_timezone'] = 'Time zone';
+$lang['contact_field_contact_type'] = 'Type';
+$lang['contact_field_groups'] = 'Groups';
+$lang['contact_card_locale'] = 'Local';
+$lang['contact_key_map'] = 'API maps key';
+$lang['contact_email_require'] = 'Mandatory email';
+$lang['contact_save_settings'] = 'Save settings';
+$lang['contact_select_contact'] = 'Select the contact';
+$lang['contact_field_display_name_widget'] = 'Contact';
+$lang['contact_field_company'] = 'Company';
+$lang['contact_select_company'] = 'Select the company';
+$lang['contact_field_job_role'] = 'Position';
+$lang['contact_select_job_role'] = 'Define the position';
+
+$lang['contact_desc_form_create'] = 'Add and / or edit your contact information.';
+$lang['help_tab_tokenize'] = 'Use Enter to define a new value.';
+
+$lang['contact_tour_register'] = 'Registering a new contact';
+
+$lang['contact_customer_save_coordinate'] = 'Save information';
+$lang['contact_customer_check_position'] = 'Mark your location on the map';
+$lang['contact_customer_geo_success'] = 'Locality registered successfully.';
+$lang['contact_placeholder'] = 'Select a contact ...';
+
+$lang['contact_settings_default_locale'] = 'Set default region';
+$lang['contact_settings_default_state'] = 'Status';
+$lang['contact_settings_default_country'] = 'Country';
+$lang['contact_settings_save_error'] = 'Error saving settings';
+$lang['contact_settings_save_success'] = 'Settings saved successfully.';
+$lang['contact_settings_show_options'] = 'View options';
+$lang['contact_set_coordinate'] = 'Set the location on the map!';
+
+$lang['contact_give_access'] = 'Add access to a user';
+$lang['contact_action_give_access'] = 'Allow access';
+$lang['contact_give_access_success'] = 'Access guaranteed with success.';
+$lang['contact_give_removed_access'] = 'Access removed successfully';
+$lang['contact_confirm_remove_access'] = 'Are you sure you want to remove access?';
